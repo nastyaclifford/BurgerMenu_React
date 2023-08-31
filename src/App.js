e17e59cbe28bbe8d8ccf30e57aff3b629a55582a
nastyaclifford <madchen95@mail.ru>
2023-08-31 12:09:47 -0600

    built example burger menu from tutorial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id="outer-container">
+      <Sidebar pageWrapId={"page-wrap"} outerContainerId={"outer-container"} />
+      <div id="page-wrap">
+        <h1>Cool Restaurant</h1>
+        <h2>Check out our offerings in the sidebar!</h2>
+      </div>
     </div>
   );
 }
